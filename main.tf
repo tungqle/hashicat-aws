efef441880101ea5e7562086d7a5592db0bd2d2e
Tung Le <tungqle@gmail.com>
2021-01-13 17:49:34 +0000

    added the second modded tag

--- main.tf
+++ main.tf
@@ -109,8 +109,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_eip" "hashicat" {
   instance = aws_instance.hashicat.id
   vpc      = true
-  tags = {
-     department = "devops"
+
   }
   
 }
@@ -130,6 +129,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
